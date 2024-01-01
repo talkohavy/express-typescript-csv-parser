d1b65d1c4869197970e24bdc9ae49d27bfd5a030
Tal Kohavy <talkohavy@gmail.com>
2024-01-01 11:58:04 +0200

    attached middlewares to the server init process

--- apps/backend/src/initServer/index.ts
+++ apps/backend/src/initServer/index.ts
@@ -1,10 +1,19 @@
 import express, { Express } from 'express';
+import { attachBaseMiddlewares } from '../middlewares/attachBaseMiddlewares.js';
+import { attachErrorMiddlewares } from '../middlewares/attachErrorMiddlewares.js';
+import { attachHttpRoutes } from '../routes/index.js';
 import './loadEnvVariables.js';
 
 const PORT = 8000;
 
 async function startServer() {
   const app: Express = express();
+
+  attachBaseMiddlewares({ app });
+
+  attachHttpRoutes(app);
+
+  attachErrorMiddlewares({ app });
 
   app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 }
